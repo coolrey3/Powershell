64bf64d7fddf4022090c03eb14aff5c73b199a75
Ray <coolrey3@gmail.com>
2018-11-20 15:57:47 -0500

    Add files via upload

---
+++ Mercedes_Raymundo_ApplicationTool.ps1
@@ -0,0 +1,51 @@
+﻿#Log Folder Path
+$logfolder = "C:\Logs"
+
+#check for Log Folder, if it does not exist create 
+if(Test-Path $logfolder) {"Log Folder Exists"} else {"Log Folder does not exist, Creating now"; "";New-Item -ItemType "directory" -Name "Logs" -Path "c:\";  } 
+
+#Start log
+Start-Transcript -Path "C:\logs\Notepad_Installation.log"
+
+#File download url 
+$download32= "https://notepad-plus-plus.org/repository/7.x/7.5.4/npp.7.5.4.Installer.exe"
+$download64= "https://notepad-plus-plus.org/repository/7.x/7.5.4/npp.7.5.4.Installer.x64.exe"
+
+#Install file path
+$install32 = "c:\npp.7.5.4.Installer.exe"
+$install64 = "c:\npp.7.5.4.Installer.x64.exe"
+
+#get computer architecture
+$osarch = Get-WmiObject win32_operatingsystem
+
+#64bit install
+if($osarch.OSArchitecture -eq "64-bit") {"64 bit architecture detected.";
+
+#check for notepad++.exe 64 bit, if it does not exist install application
+
+ if( Test-Path "C:\Program Files\Notepad++\notepad++.exe" )
+     {"" ;"notepad++ already installed"} else
+     {"Notepad++ not installed, Installing now." ; 
+     Invoke-WebRequest -Uri $download64 -OutFile $install64;
+     Start-Process $install64 -ArgumentList "/S  " } 
+     "Log Folder created and Notepad++ successfully installed:"
+     start-sleep -Seconds 5; Test-Path "C:\program files\Notepad++\notepad++.exe"}
+      
+else 
+
+#32 bit install
+{"32 Bit architecture detected."; 
+
+#check for notepad++.exe 32 bit, if it does not exist install application
+
+if( Test-Path "C:\Program Files\Notepad++\notepad++.exe" )
+     {"" ;"Notepad++ installed"} else
+     {"Notepad++ not installed, Installing now." ;
+     Invoke-WebRequest -Uri $download32 -OutFile $install32;
+     Start-Process $install32 -ArgumentList " /S" } 
+     "Log Folder created and Notepad++ successfully installed:"
+     Test-Path "C:\program files\Notepad++\notepad++.exe" 
+     }
+ 
+#stop log
+Stop-Transcript
---
+++ Mercedes_Raymundo_RegistryTool.ps1
@@ -0,0 +1,82 @@
+﻿#start log
+Start-Transcript;""
+
+#Define Paths Array
+$array = @()
+
+#Define Verify Array
+$verify = @()
+
+#Registry "Master" keys array
+$keys ="VB and VBA Program Settings", "Power Analysis"
+
+#Registry Sub Keys Array
+$skeys = "Graph","Recent"
+
+#Software registry path
+$reg = "HKLM:\Software"
+
+#Test and create "Master" keys if not found
+ForEach($key in $keys) {
+    
+    if(test-path "$reg\$key") {"$key key already exists"}  else {"$key key does not exist, creating now"; new-item "$reg" -name "$key" -itemtype "directory"}
+    $reg = "$reg\$key"
+    
+    #Registry path added to array
+    $array += New-Object psobject $reg
+    
+    #verification messages added to array
+    $vkey = "$key key verified."
+    $verify += New-Object psobject $vkey
+    $vkey    
+    }
+
+#Test and create Sub keys if not found
+ForEach($skey in $skeys) {
+    $reg = "HKLM:\Software\VB and VBA Program Settings\Power Analysis"
+    
+     if(test-path "$reg\$skey") {"$skey key already exists"}  else {"$skey key does not exist creating now" ; new-item $reg -Name "$skey"  -itemtype "directory";}
+     $reg = "$reg\$skey"
+
+     #Registry path added to array
+     $array += New-Object psobject $reg    
+
+     #verification messages added to array
+     $vskey = "$skey key verified."
+     $verify += New-Object psobject $vskey
+     $vskey
+     
+  
+     }
+           
+ #create Graph registry key properties
+
+ new-itemproperty -path $array[2] -name "gColor(1,0)" -value "1"
+ new-itemproperty -path $array[2] -name "gColor(1,1)" -value "1"
+ new-itemproperty -path $array[2] -name "gColor(1,2)" -value "1"
+ new-itemproperty -path $array[2] -name "gColor(1,3)" -value "1"
+ new-itemproperty -path $array[2] -name "gColor(1,4)" -value "1"
+ new-itemproperty -path $array[2] -name "gColor(1,5)" -value "1"
+ new-itemproperty -path $array[2] -name "gColorScheme" -value "1"
+
+  #create Recent registry key properties
+ new-itemproperty -path $array[3] -name 10 
+ new-itemproperty -path $array[3] -name 5 
+ new-itemproperty -path $array[3] -name 6 
+ new-itemproperty -path $array[3] -name 7 
+ new-itemproperty -path $array[3] -name 8 
+ new-itemproperty -path $array[3] -name 9 
+
+ #verify registry key values
+ Get-ItemProperty -path $array[2]
+ Get-ItemProperty -path $array[3]
+
+ #Validate keys created sucessfully after foreach statement
+ $vg=test-path $array[2];
+ $vr=test-path $array[3];
+
+ #Write results of script to Event Viewer
+ Write-EventLog -LogName "Application" -source "SamplePower Registry" -eventid 1 -EntryType Information -message "Tests and creates Registry Keys if they are not found. Result: $verify"   -category 1 -rawdata 10,20
+
+ #stop log
+ Stop-Transcript
